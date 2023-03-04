c6e8c6bf808282e0e084965cfbab04157b3de5b3
tejaguru888 <122436143+tejaguru888@users.noreply.github.com>
2023-03-04 16:12:54 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,4 @@
-const { verifyToken } = require('./verifyToken');
+// const { verifyToken } = require('./verifyToken');
 const {verifyTokenAndAuthorization}=require('./verifyToken')
 const {verifyTokenAndAdmin}=require('./verifyToken');
 
